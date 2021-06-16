025261e9efad04e6d78f36fb2493e84433212f21
Abner Rodrigues <40338524+rodriguesabner@users.noreply.github.com>
2021-06-16 12:44:43 -0300

    Add files via upload

--- src/controller/ContactController.js
+++ src/controller/ContactController.js
@@ -1,17 +1,29 @@
-const { sendToWebhook } = require("../util/webhook");
+const {sendToWebhook} = require("../util/webhook");
 
 module.exports = {
     async create(req, res) {
-        const { contacts } = req.body;
+        const {app_id: APP_ID} = req.headers;
+        const {contacts} = req.body;
+
+        if (!APP_ID)
+            return res.status(400).json({status: "Error", message: "Param app_id in header is required"})
+
+        if (!contacts || contacts.length <= 0)
+            return res.status(400).json({status: "Error", message: "Param contacts is required"})
 
         //webhook
-        sendToWebhook("new-contact", { contact: contacts });
+        await sendToWebhook("new-contact", {APP_ID: APP_ID, contact: contacts});
 
-        req.io.emit("new-contact", { contacts: contacts });
-        return res.status(200).json({ contacts })
+        req.io.emit("new-contact", {APP_ID: APP_ID, contacts: contacts});
+        return res.status(200).json({contacts})
     },
     async list(req, res) {
-        req.io.emit("get-contacts", null);
-        return res.status(200).json({ status: "Success", message: "Retrieving contact list..." })
+        const {app_id} = req.headers;
+
+        if (!app_id)
+            return res.status(400).json({status: "Error", message: "Param app_id in header is required"})
+
+        req.io.emit("get-contacts", {APP_ID: app_id});
+        return res.status(200).json({status: "Success", message: "Retrieving contact list..."})
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+const {config} = require("dotenv");
 const express = require("express");
-const { sendToWebhook } = require("./util/webhook");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 4747;
+const {sendToWebhook} = require("./util/webhook")
+
+config();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
     req.io = io;
@@ -15,17 +18,20 @@ app.use((req, res, next) => {
 
 io.on('connection', (sock) => {
     console.log(`User connected: ${sock.id}`);
+    sock.emit("client_first_access", {socket_id: sock.id})
 
-    sock.on("list-contacts", (data) => {
-        //webhook
-        sendToWebhook("get-contacts", { contacts: data });
+    sock.on("list-contacts", async (data) => {
+        console.log(data);
+        await sendToWebhook("get-contacts", {contacts: data});
     });
 
     sock.on('disconnect', () => {
         console.log(`User disconnected: ${sock.id}`);
     });
+
 });
 
+exports.socket = io;
 app.use(require("./routes"));
 
 server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
